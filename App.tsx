eab1a98ff5df3c952e68523fcc3e699429856cb2
byungi <zeronin2@gmail.com>
2023-12-18 00:48:28 +0900

    wip

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import WebView from 'react-native-webview'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <SafeAreaView style={{ flex: 1, backgroundColor: 'red', width: '100%' }}>
+                <WebView
+                    style={{ flex: 1, width: '100%' }}
+                    source={{
+                        uri: 'https://docs.google.com/gview?embedded=true&url=https://www.africau.edu/images/default/sample.pdf',
+                    }}
+                    scalesPageToFit={true}
+                />
+            </SafeAreaView>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
